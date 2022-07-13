577f4b3ce51c60607bdb4755a86274bbbb634eee
johnthebeloved <johnoluwale1@gmail.com>
2022-07-12 23:40:00 -0400

    Created the intro razor page

---
+++ Web-Application-Assignment/Web-Application-Assignment/Pages/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web_Application_Assignment.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
